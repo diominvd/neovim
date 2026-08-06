c46bf111c6d0bcf6acb9b5742a761a443eb1d4e1
diominvd <diomin.vd13@gmail.com>
2026-08-07 00:10:07 +0300

    feat: soft-wrap prose and strip trailing whitespace
    - disable global wrap; enable it per-filetype (markdown, text, tex, rst)
    - BufWritePre strips trailing whitespace, skipping prose and binaries

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -14,3 +14,30 @@ vim.api.nvim_create_autocmd("CursorHold", {
 		require("utils.hover").combined_hover({ focusable = false })
 	end,
 })
+
+-- Enable wrapping only for prose filetypes
+local wrap_filetypes = { "markdown", "text", "tex", "rst" }
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function(args)
+		local ft = vim.bo[args.buf].filetype
+		if vim.tbl_contains(wrap_filetypes, ft) then
+			vim.bo[args.buf].wrap = true
+			vim.bo[args.buf].linebreak = true
+			vim.bo[args.buf].breakindent = true
+		end
+	end,
+})
+
+-- Strip trailing whitespace on save (skip prose: trailing spaces are meaningful)
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		local ft = vim.bo.filetype
+		if vim.bo.binary or vim.tbl_contains(wrap_filetypes, ft) then
+			return
+		end
+		local view = vim.fn.winsaveview()
+		vim.cmd([[%s/\s\+$//e]])
+		vim.fn.winrestview(view)
+	end,
+})
